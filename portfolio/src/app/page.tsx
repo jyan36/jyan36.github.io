7dcdf49ef009854efa6f02b095f14aa8c321f40c
JackChujun <1yanjac@gmail.com>
2023-12-26 00:47:04 -0500

    Created basic layout of page, started home screen

--- portfolio/src/app/page.tsx
+++ portfolio/src/app/page.tsx
@@ -1,113 +1,165 @@
+'use client'
+
 import Image from 'next/image'
+import './globals.css';
+import React, { useState, useEffect, useRef } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faLinkedin, faGithub, faInstagram, faDiscord } from '@fortawesome/free-brands-svg-icons'
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+
+const messages = [
+  'Web Developer.',
+  'Composer and Arranger.',
+  'Math Enjoyer.',
+  '1B Computer Science Student at the University of Waterloo.',
+];
 
 export default function Home() {
+  const [isScrolled, setIsScrolled] = useState(false);
+  const currentTime = new Date();
+  const currentHour = currentTime.getHours();
+
+  // Define time ranges for different content
+  const morningRange = [6, 12];
+  const afternoonRange = [12, 18];
+  const eveningRange = [18, 24];
+
+  let greeting = '';
+
+  // Determine the time range and set the greeting accordingly
+  if (currentHour >= morningRange[0] && currentHour < morningRange[1]) {
+    greeting = 'Good morning!';
+  } else if (currentHour >= afternoonRange[0] && currentHour < afternoonRange[1]) {
+    greeting = 'Good afternoon!';
+  } else if (currentHour >= eveningRange[0] && currentHour < eveningRange[1]) {
+    greeting = 'Good evening!';
+  } else {
+    greeting = 'Hello!';
+  }
+  
+  const [displayText, setDisplayText] = useState('');
+  const [currentMessageIndex, setCurrentMessageIndex] = useState(0);
+  const [scrollPosition, setScrollPosition] = useState(0);
+
+  useEffect(() => {
+    let currentCharIndex = -1;
+    let messageIntervalId: string | number | NodeJS.Timeout | undefined;
+
+    const startMessageInterval = () => {
+      messageIntervalId = setInterval(() => {
+        currentCharIndex++;
+        setDisplayText((prevText) => prevText + messages[currentMessageIndex][currentCharIndex]);
+
+        // Stop interval when all letters are displayed
+        if (currentCharIndex === messages[currentMessageIndex].length-1) {
+          clearInterval(messageIntervalId);
+
+          // Move to the next message after a short delay
+          setTimeout(() => {
+            setDisplayText('');
+            setCurrentMessageIndex((prevIndex) => (prevIndex + 1) % messages.length);
+            startMessageInterval();
+          }, 1000); // In milliseconds
+        }
+      }, 100); // In milliseconds
+    };
+
+    startMessageInterval(); 
+
+    return () => {
+      clearInterval(messageIntervalId);
+    };
+  }, [currentMessageIndex]);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setScrollPosition(window.scrollY);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  const opacity = typeof window !== 'undefined' ? Math.min(scrollPosition / window.innerHeight * 36, 36) : 0;
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Hi! I'm Jacob, a student studying Computer Science at the University of Waterloo. &nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    
+    <div className="bg-gray-900 text-white">
+
+      <header className={`py-4 fixed top-0 w-full z-20`} style={{ backgroundColor: `rgb(8, 8, ${56-opacity})` }}>
+        <div className="container mx-auto flex justify-between items-center">
+          <a href="#" className="text-2xl font-bold">Jacob Yan</a>
+          <nav className="space-x-12">
+            <a href="#about" className="hover:text-blue-500 font-bold">
+              About
+            </a>
+            <a href="#projects" className="hover:text-blue-500 font-bold">
+              Experience
+            </a>
+            <a href="#projects" className="hover:text-blue-500 font-bold">
+              Projects
+            </a>
+            <a href="#projects" className="hover:text-blue-500 font-bold">
+              Course Notes
+            </a>
+            <a href="#contact" className="hover:text-blue-500 font-bold">
+              Resume
+            </a>
+          </nav>
+        </div>
+      </header>
+
+      <main className="z-10 flex min-h-screen flex-col items-center justify-between p-2" style={{ backgroundColor: `rgb(8, 8, 20)` }}>
+        {/* bg-gradient-to-b from-gray-800 to-gray-900 */}
+      <div className="flex items-center justify-center h-screen w-screen z-10" style={{ background: `linear-gradient(to bottom, rgb(8, 8, 56), rgb(8, 8, 20))` }}>
+        <div className="text-center">
+          <h1 className="text-2xl font-bold mb-4">{greeting}</h1>
+          <h1 className="text-4xl font-bold mb-4">My name is Jacob Yan.</h1>
+          <p className="text-lg mb-8">{"I am a " + displayText}</p>
+          <div className="flex justify-center items-center">
+            <a href="https://www.linkedin.com/in/jacob-yan/" target="_blank" rel="noopener noreferrer" className="pr-3">
+              <FontAwesomeIcon icon={faLinkedin} size="2x" />
+            </a>
+
+            <a href="https://github.com/jyan36" target="_blank" rel="noopener noreferrer" className="pr-3">
+              <FontAwesomeIcon icon={faGithub} size="2x" />
+            </a>
+
+            <a href="https://discord.com/users/598873655904174102" target="_blank" rel="noopener noreferrer" className="pr-3">
+              <FontAwesomeIcon icon={faDiscord} size="2x" />
+            </a>
+
+            <a href="https://www.instagram.com/j.yan36" target="_blank" rel="noopener noreferrer" className="pr-3">
+              <FontAwesomeIcon icon={faInstagram} size="2x" />
+            </a>
+
+            <a href="mailto:1yanjac@gmail.com" target="_blank" rel="noopener noreferrer">
+              <FontAwesomeIcon icon={faEnvelope} size="2x" />
+            </a>
+
+          </div>
         </div>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+      {/* Basic about me, APIs like chess.com, Spotify, link resume, and experience levels in languages and frameworks */}
+      <section id="about" className="additional-section">
+        <h2 className="text-2xl font-bold mb-4">About Me</h2>
+      </section>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+      <section id="projects" className="additional-section">
+        <h2 className="text-2xl font-bold mb-4">Projects</h2>
+      </section>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+      <section id="skills" className="additional-section">
+        <h2 className="text-2xl font-bold mb-4">Skills</h2>
+      </section>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      <section id="contact" className="additional-section">
+        <h2 className="text-2xl font-bold mb-4">Contact Me</h2>
+      </section>
     </main>
+    </div>
   )
 }
